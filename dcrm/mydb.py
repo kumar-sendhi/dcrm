940fc06b9f4afb1cdec95b93f3d904b5e315ff36
Kumar Krishnamurthy <kumar.sendhi@outlook.com>
2023-03-26 21:23:41 +0000

    database created

---
+++ dcrm/mydb.py
@@ -0,0 +1,15 @@
+import mysql.connector
+
+database = mysql.connector.connect(
+    host = 'localhost',
+    user = 'root',
+    passwd = 'password123'
+)
+
+
+cursorObject = database.cursor()
+
+
+cursorObject.execute('CREATE DATABASE seyonco')
+
+print("All done!")
